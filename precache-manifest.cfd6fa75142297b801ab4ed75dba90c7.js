70e6548b5d58977aade8849737d8f79b2073610d
dschounz <61173807+dschounz@users.noreply.github.com>
2020-03-12 15:09:53 -0700

    Updates

---
+++ precache-manifest.cfd6fa75142297b801ab4ed75dba90c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4524ed0a5204e52dbec9a0f445596212",
+    "url": "/LoveGenerator/index.html"
+  },
+  {
+    "revision": "edeb766a768283e2f2c2",
+    "url": "/LoveGenerator/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ca3faba02a8867354914",
+    "url": "/LoveGenerator/static/css/main.1770ca38.chunk.css"
+  },
+  {
+    "revision": "edeb766a768283e2f2c2",
+    "url": "/LoveGenerator/static/js/2.1b645f84.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/LoveGenerator/static/js/2.1b645f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3faba02a8867354914",
+    "url": "/LoveGenerator/static/js/main.59a35f83.chunk.js"
+  },
+  {
+    "revision": "4d01b392acf96757ff6d",
+    "url": "/LoveGenerator/static/js/runtime-main.8a7636ca.js"
+  }
+]);
